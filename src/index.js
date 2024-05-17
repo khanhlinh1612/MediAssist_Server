70c5c9fc73ab0362c8bdedbb46cd7e155bfd7d19
khanhlinh1612 <linhp069@gmail.com>
2024-05-17 23:56:26 +0700

    TestDeploy1

--- src/index.js
+++ src/index.js
@@ -59,5 +59,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () =>
-    console.log(`Listening on port http://localhost: ${port}`),
+    console.log(`Listening on port http://localhost:${port}`),
 );
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
+router.get('/', (req, res) => {
+    res.send('Trang chủ của MediAssist');
+});
 router.get('/totalCount', siteController.totalCount);
 router.post('/register', siteController.register);
 router.post('/login', siteController.login);
